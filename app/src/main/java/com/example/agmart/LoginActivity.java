7409160c7ba156ab1ee29c662a38cde77b879b34
Sankalpa Lokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2025-07-25 19:54:05 +0900

    complete firebase auth to sign and register

--- app/src/main/java/com/example/agmart/LoginActivity.java
+++ app/src/main/java/com/example/agmart/LoginActivity.java
@@ -2,15 +2,32 @@ package com.example.agmart;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+
+    Button Signin;
+    EditText email, password;
+
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +35,48 @@ public class LoginActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
+        auth = FirebaseAuth.getInstance();
+
+        Signin = findViewById(R.id.signinText);
+        email = findViewById(R.id.signin_email);
+        password = findViewById(R.id.signin_password);
+
         findViewById(R.id.signInDirect).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
             }
         });
+
+        Signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginUser();
+            }
+        });
+
+
     }
+    private void LoginUser() {
+        String userEmail = email.getText().toString();
+        String userPass = password.getText().toString();
+
+
+        auth.signInWithEmailAndPassword(userEmail, userPass).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                finish();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+
 }
--- app/src/main/java/com/example/agmart/RegisterActivity.java
+++ app/src/main/java/com/example/agmart/RegisterActivity.java
@@ -1,14 +1,34 @@
 package com.example.agmart;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.agmart.models.UserModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+
 public class RegisterActivity extends AppCompatActivity {
+
+    Button signup;
+    EditText name, email, password, conpassword;
+
+    FirebaseAuth auth;
+    FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +36,74 @@ public class RegisterActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
 
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        signup = findViewById(R.id.signupText);
+        name = findViewById(R.id.signup_name);
+        email = findViewById(R.id.signup_email);
+        password = findViewById(R.id.signup_password);
+        conpassword = findViewById(R.id.signup_conpassword);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreateUser();
+            }
+        });
+
+
+    }
+
+    private void CreateUser () {
+        String username = name.getText().toString();
+        String useremail = email.getText().toString();
+        String userpassword = password.getText().toString();
+        String userconpassword = conpassword.getText().toString();
+
+        if (TextUtils.isEmpty(username)) {
+            Toast.makeText(this, "Please input your name!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(useremail)){
+            Toast.makeText(this, "Please input your email!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(userpassword)) {
+            Toast.makeText(this, "Please input password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(userconpassword)) {
+            Toast.makeText(this, "Please confirm your password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+
+
+
+        else {
+
+            //create user
+            auth.createUserWithEmailAndPassword(useremail, userpassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+
+                        UserModel usermodel = new UserModel(username, useremail, userpassword);
+                        String id = task.getResult().getUser().getUid();
+                        database.getReference().child("users").child(id).setValue(usermodel);
+
+                        Toast.makeText(RegisterActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(RegisterActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 }
