4754561f369eda6c1dd113ed47378a78a82aada3
Sankalpa Lokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2025-07-25 17:09:56 +0900

    usermodel model

--- app/src/main/java/com/example/agmart/models/UserModel.java
+++ app/src/main/java/com/example/agmart/models/UserModel.java
@@ -1,4 +1,41 @@
 package com.example.agmart.models;
 
 public class UserModel {
+    String name;
+    String email;
+    String password;
+
+    public UserModel () {
+
+    }
+
+    public UserModel(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
